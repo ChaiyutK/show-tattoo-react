25bffaf0361e830f9d110489e96cadfce751aa0e
Chaiyut <95678221+ChaiyutK@users.noreply.github.com>
2022-01-10 00:35:32 +0700

    create component and InterFace

---
+++ src/components/TattooPost.js
@@ -0,0 +1,17 @@
+import './TattooPost.css';
+function TattooPost()
+{
+    return(
+        <div className="tattoo-post">
+            <div className="tattoo-post-bg" />
+            <div className="tattoo-post-cotent">
+                <img src="images/tattoo1.jpg" />
+                <h4>กวนอู</h4>
+            </div>
+        </div>
+
+
+    );
+}
+
+export default TattooPost;
---
+++ src/data/tattoos.js
@@ -0,0 +1,20 @@
+const tattoos = [
+    {
+      title : "กวนอู",
+      thumbnailUrl : "images/tattoo1.jpg"
+    },
+    {
+      title : "ลายดอก",
+      thumbnailUrl : "images/tattoo2.jpg"
+    },
+    {
+        title : "ลายเท่ๆ",
+        thumbnailUrl : "images/tattoo3.jpg"
+      },
+      {
+        title : "ลายขีดเขียน",
+        thumbnailUrl : "images/tattoo4.jpg"
+      }
+  ];
+
+export default tattoos;
