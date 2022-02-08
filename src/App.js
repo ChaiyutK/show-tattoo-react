a8df9afc516e57637101661a828550f38624f2a9
CHAIYUT KONGPRAWAT (ชัยยุทธ์ ก้องประวัติ) <5910210067@email.psu.ac.th>
2022-02-08 23:06:53 +0700

    add get me coffee in website

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
       <div className="app-grid">
       {tattooElement}
       </div>
+      <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="benzchaiyut" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FFDD00" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
     </div>
   );
 }
